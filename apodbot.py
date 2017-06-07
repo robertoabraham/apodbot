c513773d9d9f2b2aa590dfeb72be2a7fbc42827e
tinapeters <tina.peters.ys@gmail.com>
2017-06-07 15:29:18 -0400

    added datetime option

--- apodbot.py
+++ apodbot.py
@@ -1,8 +1,13 @@
 import urllib2
 import json
+from datetime import date
 
-date="2017-6-5"
-url="https://api.nasa.gov/planetary/apod?api_key=8frhOuCGozXTd7b8Jp8kfP1g9WguYRcEcsp3ET5P&date="+date
+d = date.today()
+date_string = d.isoformat()
+
+date_string = "2017-6-5"
+
+url="https://api.nasa.gov/planetary/apod?api_key=8frhOuCGozXTd7b8Jp8kfP1g9WguYRcEcsp3ET5P&date="+date_string
 js=json.loads(urllib2.urlopen(url).read())
 js['explanation']='This is an APOD image. We are awesome!'
 js['explanation']=js['explanation'].split("   ")[0]
