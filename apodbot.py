858c99dd8b9cf536c881ce79ff26c5e622fb3763
Roberto Abraham <abraham@astro.utoronto.ca>
2017-06-07 16:35:45 -0400

    Keith's stuff merged

--- apodbot.py
+++ apodbot.py
@@ -1,29 +1,38 @@
 import urllib2
 import json
+import uuid
 from datetime import date
 
 d = date.today()
 date_string = d.isoformat()
 
-date_string = "2017-6-5"
-
-url="https://api.nasa.gov/planetary/apod?api_key=8frhOuCGozXTd7b8Jp8kfP1g9WguYRcEcsp3ET5P&date="+date_string
+date="2017-6-7"
+url="https://api.nasa.gov/planetary/apod?api_key=8frhOuCGozXTd7b8Jp8kfP1g9WguYRcEcsp3ET5P&date="+date
 js=json.loads(urllib2.urlopen(url).read())
 js['explanation']=js['explanation'].split("   ")[0]
 
 from slackclient import SlackClient
 slack_token = "xoxb-193605162912-PPDqAs6RRhkeFJNnEcBGBlIu"
 sc = SlackClient(slack_token)
-output = sc.api_call(
-  "chat.postMessage",
-  channel="#apod",
-  as_user=True,
-  attachments = [{"title": js['title'], \
-                  "footer": "Scraped from https://apod.nasa.gov/",\
-                  "image_url": js['url'], \
-                  "text":js['explanation']}],
-  text="KV Testing"
-)
+
+if (js['media_type'] == "video"):
+    output = sc.api_call(
+      "chat.postMessage",
+      channel="#apod",
+      parse="full",
+      unfurl_media="true",
+      unfurl_links="true",
+      text="https://www.youtube.com/watch?v="+js['url'].split("?")[0].split("/")[-1]+"&"+str(uuid.uuid1()))
+else:
+    output = sc.api_call(
+      "chat.postMessage",
+      channel="#apod",
+      as_user=True,
+      attachments = [{"title": js['title'], \
+                      "footer": "Scraped from https://apod.nasa.gov/",\
+                      "image_url": js['url'], \
+                      "text":js['explanation']}],
+      text="KV Testing")
 
 if (output["ok"] == False):
     raise Exception('The slack bot appears to be dead')
