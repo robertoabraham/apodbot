dfebbb644e2a793691d83ee6385ef36754bebfc4
tinapeters <tina.peters.ys@gmail.com>
2017-06-07 17:23:53 -0400

    updated date format

--- apodbot.py
+++ apodbot.py
@@ -8,18 +8,40 @@ from slackclient import SlackClient
 
 p = inflect.engine()
 
-# Create nicely formatted dates
-d = date.today()
-d = datetime.date(2017,6,4)
-date_string = d.isoformat()
-my_date = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+d = date.today()            #get today's APOD
+d = datetime.date(2017,6,4) #get a specific day's APOD
+date_string = d.isoformat() #create nicely formatted dates
 
 url="https://api.nasa.gov/planetary/apod?api_key=8frhOuCGozXTd7b8Jp8kfP1g9WguYRcEcsp3ET5P&date="+date_string
 js=json.loads(urllib2.urlopen(url).read())
 js['explanation']=js['explanation'].split("   ")[0]
 
-slack_token = "FILL_ME_WITH_TOKEN"
+slack_token = "xoxb-193605162912-fV5mgy8qA3gDpPLNuWPBaUc9"
+
+para = (js['explanation'][:100] + ' ...') if len(js['explanation']) > 100 else js['explanation'][:100] 
+
 sc = SlackClient(slack_token)
+
+sc.api_call(
+  "chat.postMessage",
+  channel="#apod",
+  as_user="true",
+  text = ("*APOD FOR "+d.strftime("%B").upper()+re.sub(' +',' '," "+p.ordinal(d.strftime("%e")))+", "+d.strftime("%Y")+"*")
+)
+
+sc.api_call(
+  "chat.postMessage",
+  channel="#apod",
+  as_user="true",
+  text = ("*"+js['title'].upper()+"*")
+)
+
+sc.api_call(
+  "chat.postMessage",
+  channel="#apod",
+  as_user="true",
+  text = (para + " (For full text, click here: https://apod.nasa.gov/apod/ap"+d.strftime("%y%m%d")+".html)")
+)
 
 if (js['media_type'] == "video"):
     output = sc.api_call(
@@ -35,17 +57,8 @@ else:
       "chat.postMessage",
       channel="#apod",
       as_user=True,
-      attachments = [{"title": js['title'], \
-                      "footer": "Scraped from https://apod.nasa.gov/",\
-                      "image_url": js['url'], \
-                      "text":js['explanation']}],
-      text = ("*APOD FOR "+my_date.strftime("%B").upper()+" "+p.ordinal(my_date.strftime("%e"))+", "+my_date.strftime("%Y")+"*"))
-
-sc.api_call(
-  "chat.postMessage",
-  channel="#apod",
-  as_user="true",
-  text = ("*"+js['title'].upper()+"*")
+      attachments = [{"title": "", \
+                      "image_url": js['url']}],
 )
 
 if (output["ok"] == False):
