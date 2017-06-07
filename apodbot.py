7ee058228cf96dba6f58bf6a9f059b37f3a2d43d
Roberto Abraham <abraham@Abraham-Fortress-512-20-1000.localdomain>
2017-06-07 19:27:06 +0000

    Keith's hack

--- apodbot.py
+++ apodbot.py
@@ -1,10 +1,22 @@
+import urllib2
+import json
+
+date="2017-6-5"
+url="https://api.nasa.gov/planetary/apod?api_key=8frhOuCGozXTd7b8Jp8kfP1g9WguYRcEcsp3ET5P&date="+date
+js=json.loads(urllib2.urlopen(url).read())
+js['explanation']='This is an APOD image. We are awesome!'
+js['explanation']=js['explanation'].split("   ")[0]
+
 from slackclient import SlackClient
-
-slack_token = 'xoxb-193605162912-GHtreKXdKP3vzkBcdiEtA7Oo'
+slack_token = "xoxb-193605162912-j5LVCj5erhlsNvd4gpBok0JF"
 sc = SlackClient(slack_token)
-
 sc.api_call(
   "chat.postMessage",
   channel="#apod",
-  text="Please bring me back from the dead!"
+  attachments = [{"title": js['title'], \
+                  "footer": "Scraped from https://apod.nasa.gov/",\
+                  "image_url": js['url'], \
+                  "text":js['explanation']}],
+  text="KV Testing"
 )
+
