ec988ea34896d1d32d44a8637ad97a3c18c7fd26
Roberto Abraham <abraham@Abraham-Fortress-512-20-1000.localdomain>
2017-06-07 19:06:30 +0000

    tweak

--- apodbot.py
+++ apodbot.py
@@ -1,10 +1,10 @@
 from slackclient import SlackClient
 
-slack_token = 'xoxb-193605162912-Q54h4FX3GZ1vjpKZVRjCnhNS'
+slack_token = 'xoxb-193605162912-GHtreKXdKP3vzkBcdiEtA7Oo'
 sc = SlackClient(slack_token)
 
 sc.api_call(
   "chat.postMessage",
   channel="#apod",
-  text="Hello from Python! :tada:"
+  text="Please bring me back from the dead!"
 )
